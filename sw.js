d746c2fc2d918c27da0b9e1db6674874bfa22b23
greenfieldcj <greenfieldcj@gmail.com>
2026-09-24 09:51:13 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+/* Offline cache for Water Tracker. Bump CACHE when files change. */
+var CACHE = 'water-tracker-v1';
+var ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(ASSETS);
+    }).then(function () {
+      return self.skipWaiting();
+    }).catch(function () { /* a missing asset must not block install */ })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        return k === CACHE ? null : caches.delete(k);
+      }));
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function (hit) {
+      if (hit) return hit;
+      return fetch(event.request).then(function (res) {
+        if (res && res.status === 200 && res.type === 'basic') {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(event.request, copy); });
+        }
+        return res;
+      }).catch(function () {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
